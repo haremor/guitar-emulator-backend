5ad15421b2ff75edf74f0df32a8ba71bcd92752d
haremor <timaty6565@gmail.com>
2025-04-17 18:37:44 -0700

    opened CORS

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import auth, midi
 
 app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(midi.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)

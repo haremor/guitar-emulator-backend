68a2ae816d7a3b6c669510e8817eda700f5878c1
haremor <timaty6565@gmail.com>
2025-04-17 18:47:47 -0700

    updated CORS

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ app = FastAPI()
 app.include_router(auth.router)
 app.include_router(midi.router)
 
+allowed_origins = [
+	'http://localhost:3000'
+]
+
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=['*'],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+	CORSMiddleware,
+	allow_origins=allowed_origins,
+	allow_credentials=True,
+	allow_methods=["*"],
+	allow_headers=["*"],
 )
 
 if __name__ == "__main__":
